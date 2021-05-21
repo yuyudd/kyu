6de2499d0aaeac9c95c46d20ace5f2a8d630afe7
yuyudd <yuyudd98@naver.com>
2021-05-21 22:50:21 +0900

    first commit

--- shell.c
+++ shell.c
@@ -17,24 +17,30 @@ int main(void)
     
     char hostname[LEN_HOSTNAME +1];
     memset(hostname, 0x00, sizeof(hostname));
-    printf("username: %s\n", getpwuid(getuid())->pw_name);
+   
     gethostname(hostname, LEN_HOSTNAME);
     char pwd[LEN_PWD +1];
     getcwd(pwd,sizeof(pwd));
     pid_t pid, cpid;
+    char buf[80];
+    
     while (true) {
         char *s;
+	char buf[80];
         int len;
-       	char *s1 = hostname;	
-	printf( "호스트의 이름 :   %s\n",  s1);
-	printf( " %s\n ", pwd );
-        printf("MyShell $ ");
+       	char *s1 = hostname;
+	printf(" \x1b[33m 유저이름 : \x1b[0m %s\n", getpwuid(getuid())->pw_name);	
+	printf( "\x1b[33m 호스트의 이름 :\x1b[0m%s\n",  s1);
+	printf( "\x1b[31m %s\n \x1b[0m ", pwd );
+        printf("\x1b[32m  MyShell$ \x1b[0m  ");
         s = fgets(command, MAX_LEN_LINE, stdin);
         if (s == NULL) {
             fprintf(stderr, "fgets failed\n");
             exit(1);
         }
-        
+	
+
+
         len = strlen(command);
         printf("%d\n", len);
         if (command[len - 1] == '\n') {
@@ -64,7 +70,21 @@ int main(void)
                 fprintf(stderr, "execve failed\n");   
                 return 1;
             }
-        } 
+	    
+        }
+	printf( " \x1b[31m   종료시에는 exit를 입력!, 계속진행시에는 아무키를 누르시오 ! \x1b[0m  ");
+
+	if (fgets(buf, 80, stdin)!= NULL)
+	{
+		buf[strlen(buf)-1] = 0x00;
+		if(strcmp(buf,"exit")==0)
+			exit(0);
+	}
+
+	
     }
+
+
+
     return 0;
 }
